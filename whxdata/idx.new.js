57e3505953e049e0c88ef00416229edf16914181
LiarOnce <liaronce@hotmail.com>
2022-12-10 10:41:41 +0800

    2022.11.0.54

--- whxdata/idx.new.js
+++ whxdata/idx.new.js
@@ -1,4 +1,4 @@
 (function() {
-var index =  {"type":"index","chunkinfos":[{"type":"chunkinfo","first":"-","last":"zip_unzip","num":"3308","node":"idata1"}]};
+var index =  {"type":"index","chunkinfos":[{"type":"chunkinfo","first":"-","last":"zip_unzip","num":"3270","node":"idata1"}]};
 window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), index, { sync:true });
 })();
